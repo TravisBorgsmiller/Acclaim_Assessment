a9b5f503d613fa25e8dd07600f8c6850bc31dd31
Travis Borgsmiller <travisborgsmiller@gmail.com>
2021-02-01 05:39:30 -0600

    add changes to character controller with fix for two names on email

--- app/controllers/character_controller.rb
+++ app/controllers/character_controller.rb
@@ -1,12 +1,39 @@
 class CharacterController < ApplicationController
 
   def show
-    service = CharacterService.new
-    @character = service.get_character_data
+    @candidate = CharacterService.new.get_character_data
+    session[:current_candidate] = @candidate
   end
 
   def create
-    
+    new_character = Character.create(create_character)
+    if new_character.save!
+      flash[:success] = "#{new_character.last_name} has been recruited! Certify #{new_character.last_name} with a badge to make it offical!"
+      redirect_to :root
+    end
   end
+
+  private
+
+    def create_character
+      {
+        email: create_email + "@example.com",
+        picture: session[:current_candidate]['picture'],
+        first_name: "Recruit",
+        last_name: session[:current_candidate]['name']
+      }
+    end
+
+    def create_email
+      a = session[:current_candidate]['name'].split('')
+      b = []
+      a.each do |s|
+        if s == " "
+          s = "_"
+        end
+        b << s
+      end
+      b.join
+    end
 
 end
